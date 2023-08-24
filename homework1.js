3e8959037fb2ec08d1b4220a2cc2372cceac9215
Your Name <you@example.com>
2023-08-24 17:18:08 +0300

    homework 3: create vars

---
+++ homework1.js
@@ -0,0 +1,11 @@
+// task 1
+const myVariable = 'We are good';
+
+console.log(myVariable);
+
+// task 2
+const messageKeyHello = "Hello";
+const messageKeyWorld = "World";
+const resultString = `${messageKeyHello} ${messageKeyWorld}`;
+
+console.log(resultString);
