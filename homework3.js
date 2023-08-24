d609f5bf2c8e3600708df50d49aece8397a386b2
Your Name <you@example.com>
2023-08-24 18:46:11 +0300

    homework 3: add task 3,4

--- homework3.js
+++ homework3.js
@@ -1,7 +1,7 @@
 // task 1
-const myVariable = 'We are good';
+const sampleText = 'We are good';
 
-console.log(myVariable);
+console.log(sampleText);
 
 // task 2
 const messageKeyHello = "Hello";
@@ -9,3 +9,22 @@ const messageKeyWorld = "World";
 const resultString = `${messageKeyHello} ${messageKeyWorld}`;
 
 console.log(resultString);
+
+// task 3
+// чи вірно оголошена перевірка типу даних - і якщо ні - то змінити на вірний варіант
+//let myVariable = 'value'
+//typeof{'myVariable'}
+
+const myVariable = 'value';
+const type = typeof(myVariable);
+const varTypeResultMessage = `${type} ${myVariable}`;
+console.log(varTypeResultMessage)
+
+// task 4 
+//чи вірно оголошений код для отримання значення іншої змінної
+//( `hello ${"name"}` )
+
+const name = 'Vlad';
+const helloMessage = `Hello, ${name}`;
+
+console.log(helloMessage);
