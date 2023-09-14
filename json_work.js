0cd0ca8ba99c595e68aed5c19f404414f38bd3bd
Your Name <you@example.com>
2023-09-14 19:42:01 +0300

    covered_logger

--- json_work.js
+++ json_work.js
@@ -1,5 +1,9 @@
-import fs from 'fs-extra'
+import fs from 'fs-extra';
 import fetch from 'node-fetch';
+import log4js from 'log4js';
+
+const logger = log4js.getLogger();
+logger.level = "debug"
 
 const dataUser = {
     "username": "Frank",
@@ -14,9 +18,16 @@ async function createUser(data) {
         body: JSON.stringify(data),
         headers: {'Content-Type': 'application/json'}
         })
+        logger.debug("reaquest was send successfuly")
+
+        logger.debug("Got cheese.")
+        logger.info("Cheese is Comte");
+        logger.warn("chesse is tpp ripe!");
+        logger.fatal("Chese was breeding ground for listeria.")
 
         const dataRStatusCode = await response.status
         const responsed = await response.json()
+        logger.debug(dataRStatusCode)
         console.log(dataRStatusCode)
         console.log(responsed)
     } catch(error) {
