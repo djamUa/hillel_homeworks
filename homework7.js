ff98c55e3a5fb1ec949460cbf8c9166bfbdcc575
Your Name <you@example.com>
2023-09-18 20:50:03 +0300

    add_new_task_error_correct

--- homework7.js
+++ homework7.js
@@ -11,6 +11,12 @@
 // створити клас нащадок Worker - у якого має бути свій конструктор з оголошенням двох полів salary, work time. 
 // Також має бути два метода який виводить привітання, і каже що робота завершена.
 
+// створити метод який буде виводити в консоль повідомлення чи працює зараз працівник чи ні
+// можна використати 
+//  Date()
+// і в залежності від години виклику методу відображати текст робочі години чи ні
+// робочий день з 09:00 до 18:00
+
 
 class Human {
     constructor() {
@@ -43,11 +49,23 @@ class Worker extends Human {
     finishWork() {
         console.log('Work is finished');
     }
+
+    WorkingNow() {
+        const worksHours2 = new Date().getHours();
+
+        if  (worksHours2 >= 9 && worksHours2 < 18) {
+            console.log('on work');
+        } else {
+            console.log('off work');
+        }
+    }
 }
 
-const worker = new Worker;
+const worker = new Worker();
 
 worker.greetings(); 
 worker.finishWork();
 worker.eat();
-worker.listen(); 
+worker.listen();
+worker.WorkingNow();
+
